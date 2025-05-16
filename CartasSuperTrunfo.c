8abd1f158459a32bed5e1210abdc827b9fb41bdc
DeanMarconato04 <paulomarconato04@gmail.com>
2025-05-16 03:04:11 +0000

    Criado prinf e scanf das Cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,65 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// Desafio Super Trunfo - Países - Nivel Novato
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    //Variavel - Carta 01
+    char cidade[72], códigodacidade;
+    int populacão, pontosturisticos;
+    float area, pib;
+    //Variavel - Carta 02
+    char cidade2[72], códigodacidade2;
+    int populacão2, pontosturisticos2;
+    float area2, pib2;
+
+//Menu
+    printf ("Insira os dados das cidades nas cartas! \n")
+
+    //carta 01 - Estrutura interna da carta
+
+        printf ("Digite sua cidade: \n");
+        scanf ("%s", &cidade);
+
+        printf ("código da cidade: \n");
+        scanf ("%s", %códigodacidade;);
+
+        printf ("População: \n")
+        scanf ("%d", &populacão);
+
+        printf ("area: \n");
+        scanf ("%f", &area);
+
+        printf ("PIB: \n");
+        scanf ("%d", &pib);
+
+        printf ("Quantidade de pontos turisticos: \n");
+        scanf ("%f", &pontosturisticos);
+
+    // Carta 02 - Estrutura interna da carta
+        printf ("Digite sua cidade: \n");
+        scanf ("%s", &cidade2);
+
+        printf ("código da cidade: \n");
+        scanf ("%s", %códigodacidade2;);
+
+        printf ("População: \n")
+        scanf ("%d", &populacão2);
+
+        printf ("area: \n");
+        scanf ("%f", &area2);
+
+        printf ("PIB: \n");
+        scanf ("%d", &pib2);
+
+        printf ("Quantidade de pontos turisticos: \n");
+        scanf ("%f", &pontosturisticos2);
+
+    //Exibição das cartas
+    //Carta 01
+    printf ("CARTA 01 \n", )
+    printf ("Cidade  %s \n - Codigo da cidade: %s \n - População: %d\n - Area: %f - PIB: %f \n - Quantidade de pontos turisticos: %d \n", cidade, codigodacidade, populacão, area, pib, pontosturisticos);
+    printf ("Cidade  %s \n - Codigo da cidade: %s \n - População: %d\n - Area: %f - PIB: %f \n - Quantidade de pontos turisticos: %d \n", cidade2, codigodacidade2, populacão, area, pib, pontosturisticos2);
+
 
     return 0;
 }
